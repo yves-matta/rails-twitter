d68b2cb209ed68957c46b0a5f82d1eb3ef26e47f
YvesJudeMatta <Yves.Matta7@gmail.com>
2015-01-07 20:45:32 +0000

    Activate seed users and fixtures by default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token
-  before_save downcase_email
-  before_create create_activation_digest
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -40,13 +40,15 @@ class User < ActiveRecord::Base
   end
   
   private
+    # Converts email to all lower-case.
     def downcase_email
       self.email = email.downcase
     end
-    
+
+    # Creates and assigns the activation token and digest.
     def create_activation_digest
-      self.activation_token = User.new_token
-      self.activation_digest = User.User.digest(activation_token)
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 
 end
