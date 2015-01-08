4df0416598097750a7897a73eeab1c1bca7c6aaa
YvesJudeMatta <Yves.Matta7@gmail.com>
2015-01-08 06:44:38 +0000

    Generate intergration test for password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver_now
   end
   
-  # Returns true if the password has expires
+  # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
