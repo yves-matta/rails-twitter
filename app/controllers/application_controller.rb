dd266c19f88b5d9b6e46310961d26e66e3068e35
YvesJudeMatta <Yves.Matta7@gmail.com>
2015-01-08 21:14:39 +0000

    Add authorization to the Microposts controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   def hello
     render text: "Hello World!"
   end
+  
+  private
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,9 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  
+  def create
+  end
+  
+  def destory
+  end
 end
