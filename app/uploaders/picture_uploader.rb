27ed67df8892f7d6fc0eaf3d3b6d57daf4e1da38
YvesJudeMatta <Yves.Matta7@gmail.com>
2015-01-10 03:20:14 +0000

    Add user microposts

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,16 +1,19 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-  process resize_to_limit: [400,400]
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
+  process resize_to_limit: [400,400]
+  
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
